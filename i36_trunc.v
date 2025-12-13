ddb936ad25952b8c3667fd8a9cc2fa68168e93ec
amateurstock <immanuellefyan@gmail.com>
2025-12-13 15:55:44 +0800

    made truncator output two signals instead of one.
    forgot that i can't just use a saturated output as inputs for my
    feedback multipliers. gonna work on writing new feedback multipliers
    too.

--- i36_trunc.v
+++ i36_trunc.v
@@ -1,15 +1,17 @@
 module i36_trunc(
     input signed [35:0] A,
-    output reg signed [15:0] B
+    output reg signed [15:0] B_sat,
+    output reg signed [17:0] B
 );
 
 wire signed [35:0] shifted_inter = (A >>> 14);
 wire signed [21:0] shifted = shifted_inter[21:0];
 
 always @(*) begin
-    if (shifted > 22'sd32767) B = 16'sd32767;
-    else if (shifted < -22'sd32768) B = -16'sd32768;
-    else B = $signed(shifted[15:0]);
+    B = $signed(shifted[17:0]);
+    if (shifted > 22'sd32767) B_sat = 16'sd32767;
+    else if (shifted < -22'sd32768) B_sat = -16'sd32768;
+    else B_sat = $signed(shifted[15:0]);
 end
 
 endmodule
